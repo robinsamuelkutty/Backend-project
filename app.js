fbf59610e84f1270be826d47f43e8c5b3284de47
Robin Samuelkutty <114683113+robinsamuelkutty@users.noreply.github.com>
2025-04-28 12:01:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ db.connect((err)=>{
 
 var app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials/'}))
@@ -30,18 +30,16 @@ app.use(session({secret:"Key",cookie:{maxAge:5724}}))
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
